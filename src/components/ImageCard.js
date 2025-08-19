038862aed9bd028b25d80199e5e841c711624252
Daiane <daiane@MacBook-Air-von-medialabel.local>
2025-08-19 17:29:33 +0200

    first commit

---
+++ src/components/ImageCard.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const ImageCard = ({ image }) => {
+
+
+  const tags = image.tags.split(',').slice(0, 3);
+
+
+  return (
+
+    <div className="bg-white rounded-lg shadow">
+
+      <div className="w-full h-96">
+        <img
+          src={image.webformatURL}
+          alt="Imagem aleatória"
+          className="w-full h-full object-cover rounded-t-lg"
+        />
+
+      </div>
+
+      <div className="p-4">
+
+        <div className="px-6 py-4">
+          <div className="font-bold text-purple-500 text-xl">
+            Photo by {image.user}
+          </div>
+          <ul>
+            <li>
+              <strong>Views: </strong>
+              {image.views}
+            </li>
+            <li>
+              <strong>Downloads: </strong>
+              {image.downloads}
+            </li>
+            <li>
+              <strong>Likes: </strong>
+              {image.likes}
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">
+          {tags.map((tag, index) => (
+
+            <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ">
+              #{tag}
+            </span>
+
+          ))}
+
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default ImageCard;
